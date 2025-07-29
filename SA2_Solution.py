ef5c08be2dbb13bb5750e9dd341b0346a7525af0
schooli987 <curriculumfiles@schooli.online>
2025-07-29 18:10:04 +0530

    Update SA2_Solution.py

--- SA2_Solution.py
+++ SA2_Solution.py
@@ -1,31 +1,31 @@
 # Tuple with current order of runners
-starting_order = ("Ravi", "Zara", "Imran", "Anaya", "Liam")
+starting_order = ("Raju", "Kavya", "Issac", "Arun", "Lizy")
 
-# Dictionary of runner heights
-heights = {
-    "Anaya": 150,
-    "Ravi": 165,
-    "Zara": 145,
-    "Imran": 170,
-    "Liam": 160
+# Dictionary of runner weights in Kgs
+weights = {
+    "Arun": 52,
+    "Raju": 60,
+    "Kavya": 55,
+    "Issac": 65,
+    "Lizy": 58
 }
 
 # Step 1: Convert tuple into a list for sorting
 runner_list = []
 for name in starting_order:
-    runner_list.append((name, heights[name]))
+    runner_list.append((name, weights[name]))
 
-# Step 2: Apply Selection Sort based on height
+# Step 2: Apply Selection Sort based on weight (Descending order)
 n = len(runner_list)
 for i in range(n):
-    min_index = i
+    max_index = i
     for j in range(i + 1, n):
-        if runner_list[j][1] < runner_list[min_index][1]:
-            min_index = j
+        if runner_list[j][1] > runner_list[max_index][1]:  # descending
+            max_index = j
     # Swap
-    runner_list[i], runner_list[min_index] = runner_list[min_index], runner_list[i]
+    runner_list[i], runner_list[max_index] = runner_list[max_index], runner_list[i]
 
 # Step 3: Display new sorted order
-print("New starting line-up (by height):")
-for name, height in runner_list:
-    print(name, "-", height, "cm")
+print("New starting line-up (by weight, descending):")
+for name, weight in runner_list:
+    print(name, "-", weight, "kg")
